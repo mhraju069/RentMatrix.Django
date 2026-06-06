83412e50535b5e301623bca87c6138414e4d89f9
Raju <mhraju069@gmail.com>
2026-06-06 14:39:19 +0600

    Add: bookings model for book property

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from property.models import Property
+from django.conf import settings
 
 # Create your models here.
+
+class Booking(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)    
+    guest_count = models.IntegerField()
+    check_in = models.DateField()
+    check_out = models.DateField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
