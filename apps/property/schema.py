dca3733ecf6d66780a9a8f02ca9f933641387608
Raju <mhraju069@gmail.com>
2026-06-06 12:03:39 +0600

    Add: Property schemas

---
+++ apps/property/schema.py
@@ -0,0 +1,45 @@
+import msgspec
+from typing import Optional,List
+
+class UserSchema(msgspec.Struct):
+    name: str
+    image: Optional[str] = None
+
+
+class PropertyGallerySchema(msgspec.Struct):
+    type: str
+    file: str
+
+
+class PropertyAmenitySchema(msgspec.Struct):
+    name: str
+
+
+class ReviewSchema(msgspec.Struct):
+    rating: float
+    review: str
+    user: UserSchema
+    created_at: str
+
+
+class PropertyDetailSchema(msgspec.Struct):
+    name: str
+    about: str
+    price: float
+    owner: UserSchema
+    bathroom: int
+    bedroom: int
+    size: str
+    type : str
+    status: str
+    verified: bool
+    review_count: str
+    average_rating : str
+    address : str
+    latitude : float
+    longitude : float
+    amenities : Optional[List[PropertyAmenitySchema]] = None
+    gallery: Optional[List[PropertyGallerySchema]] = None
+    reviews : Optional[List[ReviewSchema]] = None
+
+
